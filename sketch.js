313ed09f5de1369f01c7c50faaff2230430bdb5b
trinityinstore <44550336+trinityinstore@users.noreply.github.com>
2018-11-25 23:15:27 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,43 +1,36 @@
-// Create a client instance
-var client = new Paho.MQTT.Client(location.hostname, Number(location.port), "clientId");
+//Using the HiveMQ public Broker, with a random client Id
+var client = new Messaging.Client("broker.mqttdashboard.com", 8000, "myclientid_" + parseInt(Math.random() * 100, 10));
 
-// set callback handlers
-client.onConnectionLost = onConnectionLost;
-client.onMessageArrived = onMessageArrived;
+//Gets  called if the websocket/mqtt connection gets disconnected for any reason
+client.onConnectionLost = function (responseObject) {
+		//Depending on your scenario you could implement a reconnect logic here
+		alert("connection lost: " + responseObject.errorMessage);
+};
 
-// connect the client
-client.connect({onSuccess:onConnect});
+//Gets called whenever you receive a message for your subscriptions
+client.onMessageArrived = function (message) {
+		//Do something with the push message you received
+		$('#messages').append('<span>Topic: ' + message.destinationName + '  | ' + message.payloadString + '</span><br/>');
+};
 
+//Connect Options
+var options = {
+		timeout: 3,
+		//Gets Called if the connection has sucessfully been established
+		onSuccess: function () {
+				alert("Connected");
+		},
+		//Gets Called if the connection could not be established
+		onFailure: function (message) {
+				alert("Connection failed: " + message.errorMessage);
+		}
+};
 
-// called when the client connects
-function onConnect() {
-  // Once a connection has been made, make a subscription and send a message.
-  console.log("onConnect");
-  client.subscribe("World");
-  message = new Paho.MQTT.Message("Hello");
-  message.destinationName = "World";
-  client.send(message);
-}
-
-// called when the client loses its connection
-function onConnectionLost(responseObject) {
-  if (responseObject.errorCode !== 0) {
-    console.log("onConnectionLost:"+responseObject.errorMessage);
-  }
-}
-
-// called when a message arrives
-function onMessageArrived(message) {
-  console.log("onMessageArrived:"+message.payloadString);
-}
-
-
-function setup() {
-	createCanvas(600, 400);
-}
-
-function draw() {
-	background(51);
-	fill(255,0,0);
-	ellipse(100, 100, 25,25);
+//Creates a new Messaging.Message Object and sends it to the HiveMQ MQTT Broker
+var publish = function (payload, topic, qos) {
+		//Send your message (also possible to serialize it as JSON or protobuf or just use a string, no limitations)
+		var message = new Messaging.Message(payload);
+		message.destinationName = topic;
+		message.qos = qos;
+		client.send(message);
 }
